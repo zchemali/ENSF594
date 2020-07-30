3f60a87755d2c1e7c5f46d85d4eefc645fbaf0fb
Ziad <zchemali@ucalgary.ca>
2020-07-29 23:46:51 -0600

    Q-2 modified-added divide and conquer algorithm

--- Assignment_3/src/MajorityElement.java
+++ Assignment_3/src/MajorityElement.java
@@ -1,9 +1,37 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class MajorityElement {
+	public int counter (int[] list,int num) {
+		int count=0;
+		for (int i=0;i<list.length;i++) {
+			if(list[i]==num) {
+				count++;
+			}
+		}
+		return count;
+	}
+int check=0;
+public int majorityDivide(int[] list, int low, int high) {
+    if (low==high) {
+        return list[low];
+    }
 
+    int mid = (high+low)/2;
+    int left = majorityDivide(list, low, mid);
+    int right = majorityDivide(list, mid+1, high);
+   
+    
+    int leftCount = counter(list, left);
+    int rightCount = counter(list, right);
+    if((leftCount>list.length/2)||(rightCount>list.length/2)) {
+    	check=1;
+    }
+	return check;
+   
+}
 	public int checkMajority(int[] list) {
 		HashMap <Integer,Integer> map =new HashMap<Integer, Integer>();
 		for (int i=0 ;i<list.length;i++) {
@@ -33,8 +61,10 @@ public class MajorityElement {
 			System.out.println("Enter element "+(i+1)+" of this list");
 			list[i]=scanner.nextInt();
 		}
+		System.out.println("------------------------------------------");
+		System.out.println("Majority using Divide and Conquer: "+ me.majorityDivide(list, 0, n-1));
 		
-		System.out.println("Majority element check "+me.checkMajority(list));
+		System.out.println("Majority element check using HashMap O(n) "+me.checkMajority(list));
 	}
 
 }
