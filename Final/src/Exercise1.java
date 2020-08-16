56fe316d733127268e7e86a4e146ccb57105e4c6
Ziad <zchemali@ucalgary.ca>
2020-08-16 02:53:41 -0600

    Q1 is done- need to comment/write report

--- Final/src/Exercise1.java
+++ Final/src/Exercise1.java
@@ -1,13 +1,17 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Random;
 
 public class Exercise1 {
 	/**
-	 * Swapping two elemnts at a time, last elemnts every pass is the biggest
-	 * my own implementaion  by understanding how the algorithm work
+	 * Swapping two elements at a time, last elements every pass is the biggest
+	 * my own implementation  by understanding how the algorithm work
 	 * @param array
 	 */
-	public void BubbleSort(int[] array) {
+	public void bubbleSort(int[] array) {
 	int n=array.length;
 	while(n>0) {
 		for (int i=0;i<n-1;i++) {
@@ -24,12 +28,12 @@ public class Exercise1 {
 		
 	}
 	/**
-	 * two loops , first loop goesfrom 1st to length-1 of the array and choose the elments and put it in 
+	 * two loops , first loop goes from 1st to length-1 of the array and choose the elments and put it in 
 	 * the correct position by using a second loop
 	 * second loop goes backward i-1 to 0 and puts the elment in correct posiotn
 	 * @param array
 	 */
-	public void InsertionSort (int[] array) {
+	public void insertionSort (int[] array) {
 		
 		for (int i=1 ;i<array.length;i++) {
 			int temp=array[i];
@@ -42,51 +46,170 @@ public class Exercise1 {
 		}
 		
 	}
-	public void MergeSort(int[] array) {
+	/**
+	 * from https://www.geeksforgeeks.org/merge-sort/
+	 * The merge sort is divide and coqur, kepp dividing unti lyou end up with 2 or 1 elments
+	 * then merge them
+	 * @param arrayint
+	 * @param l
+	 * @param r
+	 */
+	public void mergeSort(int[] array ,int l, int r) {
+		
+		if( r>l) {
+			int m=(l+r)/2;
+			mergeSort(array, l, m);// left section of the array
+			mergeSort(array, m+1, r);//right section
+			//invoke Merge when elemnts of both left/right section are 1 or 2
+			merge(array,l,m,r);
+		}
 		
 	}
-	public void QuickSort(int [] array) {
+	/**
+	 * This method simply compares the two sub arrays (l/r) and merge them together
+	 * @param array
+	 * @param l
+	 * @param m
+	 * @param r
+	 */
+	public void merge (int[] array, int l,int m, int r) {
+		int[] leftArray=new int[m-l+1];
+		int[] rightArray=new int[r-m];
+		//Filling left arary
+		for (int i=0;i<leftArray.length;i++) {
+			leftArray[i]=array[l+i];
+		}
+		//filling right array
+		for (int i=0;i<rightArray.length;i++) {
+			rightArray[i]=array[m+1+i];//+1 since the right array starts at m+1
+		}
+//		System.out.println("LEFT"+Arrays.toString(leftArray)+"  Right"+Arrays.toString(rightArray));
+		//compare and merge
+		int i=0,j=0;
+		int k=l;//actual array
+		while(i<leftArray.length && j<rightArray.length) {
+			if(leftArray[i]<=rightArray[j]) {
+				array[k]=leftArray[i];
+				i++;
+			}
+			else {
+				array[k]=rightArray[j];
+				j++;
+			}
+			k++;
+		}
+		
+		//copy remainder of left array if any Note: in case you have one elemnt only
+		while(i<leftArray.length) {
+			array[k]=leftArray[i];
+			i++;
+			k++;
+		}
+		//copy remainder of left array if any Note: incase you have one elemnt only
+		while (j<rightArray.length) {
+			array[k]=rightArray[j];
+			j++;
+			k++;
+		}
+		
+	
 		
 	}
-	public void fillArray(int [] array,int type) {
+	/**
+	 * https://www.geeksforgeeks.org/quick-sort/
+	 * @param array
+	 * @param low
+	 * @param high
+	 */
+	public void quickSort(int [] array ,int low, int high) {
+		if(low<high) {
+			int index =partition(array, low, high);
+			if(index>0 &&(index)<high)
+			{quickSort(array, low, index-1);
+			quickSort(array, index+1, high);}
+		}
+		
+	}
+	public int partition (int[] array, int low, int high) {
+		double pivot =array[high];
+		int i=low;// i is the last element that swapping has occured 
+		for (int j = low; j < high; j++) {
+			if (array[j] <= pivot) {
+
+				int temp = array[j];
+				array[j] = array[i];
+				array[i] = temp;
+				i++;
+
+			}
+		}
+			//swapping the pivot with the element at i
+		   int temp = array[i]; 
+	        array[i] = (int)pivot; 
+	        array[high] = temp; 
+		return i;
+		
+	}
+	public void writeFile (int[] array,String order,String algorithm, int size,double time,String outputfile) {
+		StringBuffer sb =new StringBuffer();
+		sb.append("Array of size "+size+" is filled at "+order+" order \n");
+		sb.append("Time needed using "+algorithm+" sort is: "+time+" s\n");
+		sb.append("The sorted Array:\n");
+		for(int i=0;i<array.length;i++) {
+			sb.append(array[i]+"\n");
+		}
+		File file =new File (outputfile);
+		try {
+			FileWriter fw =new FileWriter(file);
+			BufferedWriter bw=new  BufferedWriter (fw);
+			bw.write(sb.toString());
+			bw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+//		System.out.println(sb.toString());
+	}
+	public void fillArray(int [] array,String type) {
 		System.out.println("Filling Array");
 		Random r =new Random();
 		int flag=r.nextBoolean() ? 1:-1;
 		array[0]=(int)(Math.random()*r.nextInt(1000)*flag//setting first element 
 				);
-		if (type==0)
+		if (type.equals("ascending"))
 			for (int i=1;i<array.length;i++) {
 				array[i]=(int)(Math.random()*r.nextInt(100)+ array[i-1]);
 			}
-		else if(type==1)
-			{System.out.println("GGG"+array[0]+"\n");
+		else if(type.equals("descending"))
+			{
 			for (int i=1;i<array.length;i++) {
 				array[i]=(int)(array[i-1]-Math.random()*r.nextInt(100));
 			}}
 		else {
-			System.out.println("TYPE 2"+type);
 			for (int i=1;i<array.length;i++) {
 				flag=r.nextBoolean() ? 1:-1;
-				array[i]=(int)(Math.random()*r.nextInt(100)*flag);
+				array[i]=(int)(Math.random()*r.nextInt(1000)*flag);
 			}
 			
 			
 			
 		
-	}}
+	}
+	}
 	
-
 	public static void main(String[] args) {
-		String outputfile;
+		String outputfile = null,algorithm = null,order = null;
 		double time = 0;
-		int size = 0,type =-1,algorithm=-1;
+		int size = 0;
 			
 			if(args[0].equalsIgnoreCase("ascending"))
-					type=0;
+					order="ascending";
 			else if(args[0].equalsIgnoreCase("descending") )
-					type=1;
+					order="descending";
 			else if(args[0].equalsIgnoreCase("random"))
-					type=2;
+					order="random";
 		else {
 			System.err.println("Use one of the following strings for Order: ascending, descending, random"+"\n Aborting...");
 			System.exit(0);
@@ -106,52 +229,57 @@ public class Exercise1 {
 		
 		
 		if( args[2].equalsIgnoreCase("bubble") )
-			 algorithm=0;
+			 algorithm="bubble";
 		else if (args[2].equalsIgnoreCase("insertion") )
-		algorithm=1;
+			algorithm="insertion";
 		else if (args[2].equalsIgnoreCase("merge") )
-			algorithm=2;
+			algorithm="merge";
 		else if (args[2].equalsIgnoreCase("quick"))
-			algorithm=3;
+			algorithm="quick";
 		else {
 			System.err.println("Use one of the following strings for Algorithm: bubble, insertion, merge, quick"+"\nAborting...");
 			System.exit(0);
 		}
 		
-	
-		 outputfile=args[3];
+		if(args[3].contains(".txt")) {
+		 outputfile=args[3];}
+		else {
+			System.err.println("Outputfile must have .txt at the end\nAborting...");
+			System.exit(0);
+		}
 		 //create and fill array
 		 int[] array=new int[size];
 		Exercise1 ex=new Exercise1();
-		ex.fillArray(array, type);
+		ex.fillArray(array, order);
 		//calling sort method
 		System.out.println("Sorting ....");
-		if(algorithm==0) {
+		if(algorithm.equals("bubble")) {
 			long start =System.nanoTime();
-			ex.BubbleSort(array);
+			ex.bubbleSort(array);
 			time=(System.nanoTime()-start)/Math.pow(10, 9);
 		}
-		else if (algorithm==1) {
+		else if (algorithm.equals("insertion")) {
 			long start =System.nanoTime();
-			ex.InsertionSort(array);
+			ex.insertionSort(array);
 			time=(System.nanoTime()-start)/Math.pow(10, 9);
 		}
-		else if (algorithm==2) {
+		else if (algorithm.equals("merge")) {
 			long start =System.nanoTime();
-			ex.MergeSort(array);
+			ex.mergeSort(array,0,size-1);
 			time=(System.nanoTime()-start)/Math.pow(10, 9);
 		}
-		else if (algorithm==3) {
+		else if (algorithm.equals("quick")) {
 			long start =System.nanoTime();
-			ex.QuickSort(array);
+			ex.quickSort(array,0,size-1);
 			time=(System.nanoTime()-start)/Math.pow(10, 9);
 		}
-		
+		System.out.println("Sorting Done \nWriting to file");
 		//TODO write to file
-		
-		System.out.println(Arrays.toString(array));
-		System.out.println("DONE");
-		System.out.println("Total time in sec is "+ time);
+		ex.writeFile(array,order,algorithm,size,time,outputfile);
+		System.out.println("Success!");
+//		System.out.println(Arrays.toString(array));
+//		System.out.println("DONE");
+//		System.out.println("Total time in sec is "+ time);
 	}
 
 }
